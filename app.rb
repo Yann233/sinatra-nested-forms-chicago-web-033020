64fecf28c0c1eeb06c3de27a5e4773e51a0ef8f7
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-04-22 17:10:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,14 +15,14 @@ module FormsLab
   post '/pirates' do
     @pirate = Pirate.new(params[:pirate])
 
-    params[:pirate][:ships].each do |details|
-      Ship.new(details)
-    end
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
 
     @ships = Ship.all
-
+    
     erb :'pirates/show'
-  end
-
+    end
+    
   end
 end
